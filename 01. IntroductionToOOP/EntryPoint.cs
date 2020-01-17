e5f59b6ec2a3b70e9c026b6a9f56e6dcbb56ab51
Lockeheede <prescott.lawani@gmail.com>
2020-01-16 22:08:56 -0600

    Added notes on how classes, fields and properties

--- 01. IntroductionToOOP/EntryPoint.cs
+++ 01. IntroductionToOOP/EntryPoint.cs
@@ -5,24 +5,24 @@
         static void Main()
         {
         //Within the main class, you make an instance of your class
-        //This is call instanciating 
+        //This is call instanciating (There is a Thief.cs in the Solution Explorer)
         Thief playerOne = new Thief();
 
         //To access the variables of the new instance, you use the
         //dot operator
-        playerOne.name = "Lockes";
-        playerOne.level = 5;
-        playerOne.damage = playerOne.level * 3; //Note for game, this character will have 3 damage added per level 
-        playerOne.healthPoints = playerOne.level * 50; //50 health per level
-        System.Console.WriteLine(playerOne.healthPoints);
+        playerOne.Name = "Lockes";
+        playerOne.Level = 5;
+        playerOne.Damage = playerOne.Level * 3; //Note for game, this character will have 3 damage added per level 
+        playerOne.HealthPoints = playerOne.Level * 50; //50 health per level
+        System.Console.WriteLine(playerOne.HealthPoints);
 
         //You can instanciate with another object, but you can create new variables
         Thief enemyOne = new Thief();
 
-        enemyOne.name = "Ratface";
-        enemyOne.level = 10;
-        enemyOne.damage = enemyOne.level * 5;
-        enemyOne.healthPoints = enemyOne.level * 20;
+        enemyOne.Name = "Ratface";
+        enemyOne.Level = 10;
+        enemyOne.Damage = enemyOne.Level * 5;
+        enemyOne.HealthPoints = enemyOne.Level * 20;
 
         //Make a new class file by right clicking the project in the solution explorer
         //Choose Add > Class. Make sure your class is the same name as the class you created
--- 01. IntroductionToOOP/Thief.cs
+++ 01. IntroductionToOOP/Thief.cs
@@ -4,9 +4,23 @@ public class Thief
 {
     //The access modifiers need to be public 
     //So the class can be accessed by methods, etc
-    public int level;
-    public string name;
-    public int damage;
-    public int healthPoints;
+    private int level;
+    private string name;
+    private int damage;
+    private int healthPoints;
+    //These variables are called fields, or internal logic
+    //Meaning they should only be accessed in the class
+    //Also meaning they should be private
 
+    //Note: Alt+Shift and keys up down, etc is a mass edit
+
+    //Properties are variables that connect the fields to their
+    //External logic versions outside the class
+    //Remember to capitalize properities normally, and use getters and setters
+    //The shortcut for properities is prop+TAB+TAB
+
+    public int Level { get; set; }
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public int HealthPoints { get; set; }
 }
