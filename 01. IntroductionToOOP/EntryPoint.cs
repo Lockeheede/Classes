b4e93f5b85febb0725667bec189d59b455226e9e
Lockeheede <prescott.lawani@gmail.com>
2020-01-16 21:10:11 -0600

    Add project files.

---
+++ 01. IntroductionToOOP/EntryPoint.cs
@@ -0,0 +1,31 @@
+﻿
+
+    public class EntryPoint
+    {
+        static void Main()
+        {
+        //Within the main class, you make an instance of your class
+        //This is call instanciating 
+        Thief playerOne = new Thief();
+
+        //To access the variables of the new instance, you use the
+        //dot operator
+        playerOne.name = "Lockes";
+        playerOne.level = 5;
+        playerOne.damage = playerOne.level * 3; //Note for game, this character will have 3 damage added per level 
+        playerOne.healthPoints = playerOne.level * 50; //50 health per level
+        System.Console.WriteLine(playerOne.healthPoints);
+
+        //You can instanciate with another object, but you can create new variables
+        Thief enemyOne = new Thief();
+
+        enemyOne.name = "Ratface";
+        enemyOne.level = 10;
+        enemyOne.damage = enemyOne.level * 5;
+        enemyOne.healthPoints = enemyOne.level * 20;
+
+        //Make a new class file by right clicking the project in the solution explorer
+        //Choose Add > Class. Make sure your class is the same name as the class you created
+        //For example, this Thief class will be named Thief.cs
+        }
+    }
---
+++ 01. IntroductionToOOP/Thief.cs
@@ -0,0 +1,12 @@
+﻿
+//First you make the class
+public class Thief
+{
+    //The access modifiers need to be public 
+    //So the class can be accessed by methods, etc
+    public int level;
+    public string name;
+    public int damage;
+    public int healthPoints;
+
+}
